527c9611195db32f255c74ccaaa1eff78bc3e931
ashwinganesh01 <ashwinkidayanattu@gmail.com>
2025-11-23 15:53:55 +0400

    Revert "Solution 2-Biography"
    This reverts commit e2710683f11a855a8fcdf1c05243fb3675ce72fa.

--- Exercise 03 - Biography/ashwinsolution3-Biography.py
+++
@@ -1,7 +0,0 @@
-name=input("Enter your full name: ")
-hometown=input("Enter your hometown: ")
-age=input("Enter your age: ")  #Asking users for input
-while age.isdigit()==False:  #To check if age is entered in numbers
-    print("Invalid input")
-    age=input("Enter your age in numbers: ")
-print("Name: " ,name, "\nHometown: ", hometown,"\nAge: ",age)
