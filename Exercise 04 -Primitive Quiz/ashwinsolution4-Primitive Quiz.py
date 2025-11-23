b4f295ee50d232a0b26c9a25e263390db60d2863
ashwinganesh01 <ashwinkidayanattu@gmail.com>
2025-11-23 16:05:02 +0400

    Solution 4 -Primitive Quiz
    This program is a 10-question capital city quiz. It stores the correct answers in variable ,asks the user each question, converts the users answer to lowercase for making it case insensitive and then displays whether each answer is correct or wrong

---
+++ Exercise 04 -Primitive Quiz/ashwinsolution4-Primitive Quiz.py
@@ -0,0 +1,60 @@
+first_answer="paris"
+second_answer="berlin"
+third_answer="rome"
+fourth_answer="madrid"
+fifth_answer="london"                  #Setting variable for all answers
+sixth_answer="amsterdam"
+seventh_answer="lisbon"
+eighth_answer="vienna"
+ninth_answer="prague"
+tenth_answer="warsaw"
+a=input("1.Enter the capital of France: ")  #Asking the questions 
+if a.lower()==first_answer:          #Making it not case sensitive
+    print("The first answer is correct")
+else:
+    print("Answer is Wrong")
+b=input("2.Enter the capital of Germany: ")
+if b.lower()==second_answer:
+    print("The second answer is correct")
+else:
+    print("Answer is Wrong")
+c=input("3.Enter the capital of Italy: ")
+if c.lower()==third_answer:
+    print("The third answer is correct")
+else:
+    print("Answer is Wrong")
+d=input("4.Enter the capital of Spain: ")
+if d.lower()==fourth_answer:
+    print("The fourth answer is correct")
+else:
+    print("Answer is Wrong")
+e=input("5.Enter the capital of United Kingdom: ")
+if e.lower()==fifth_answer:
+    print("The fifth answer is correct")
+else:
+    print("Answer is Wrong")
+f=input("6.Enter the capital of Netherlands: ")
+if f.lower()==sixth_answer:
+    print("The sixth answer is correct")
+else:
+    print("Answer is Wrong")
+g=input("7.Enter the capital of Portugal: ")
+if g.lower()==seventh_answer:
+    print("The seventh answer is correct")
+else:
+    print("Answer is Wrong")
+h=input("8.Enter the capital of Austria: ")
+if h.lower()==eighth_answer:
+    print("The eighth answer is correct")
+else:
+    print("Answer is Wrong")
+i=input("9.Enter the capital of Czech Republic: ")
+if i.lower()==ninth_answer:
+    print("The ninth answer is correct")
+else:
+    print("Answer is Wrong")
+j=input("10.Enter the capital of Poland: ")
+if j.lower()==tenth_answer:
+    print("The tenth answer is correct")
+else:
+    print("Answer is Wrong")
